c7a43a6858f717b3a16281c286f9caec52832598
JelloBeans <jello.beans.sharp@gmail.com>
2015-10-20 11:27:03 +0100

    Add Kindred class and updated cs project file

---
+++ SFXChallenger/Champions/Kindred.cs
@@ -0,0 +1,62 @@
+﻿using SFXChallenger.Abstracts;
+using SFXChallenger.Enumerations;
+
+namespace SFXChallenger.Champions
+{
+    class Kindred : Champion
+    {
+        protected override ItemFlags ItemFlags
+        {
+            get {  }
+        }
+
+        protected override ItemUsageType ItemUsage
+        {
+            get {  }
+        }
+
+        protected override void OnLoad()
+        {
+        }
+
+        protected override void SetupSpells()
+        {
+        }
+
+        protected override void AddToMenu()
+        {
+        }
+
+        protected override void OnPreUpdate()
+        {
+        }
+
+        protected override void OnPostUpdate()
+        {
+        }
+
+        protected override void Combo()
+        {
+        }
+
+        protected override void Harass()
+        {
+        }
+
+        protected override void LaneClear()
+        {
+        }
+
+        protected override void JungleClear()
+        {
+        }
+
+        protected override void Flee()
+        {
+        }
+
+        protected override void Killsteal()
+        {
+        }
+    }
+}
